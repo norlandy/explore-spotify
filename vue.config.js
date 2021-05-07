e5c90df2c5748f499eb056aa4e46dcc8577aa18f
Edgar <lobachevsky.edgar@gmail.com>
2021-05-07 15:20:46 +0500

    fixed service worker

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,48 @@ module.exports = {
 	transpileDependencies: ['vuetify'],
 
 	pwa: {
+		workboxOptions: {
+			clientsClaim: true,
+			runtimeCaching: [
+				// static resources
+				{
+					handler: 'StaleWhileRevalidate',
+					urlPattern: ({request}) =>
+						request.destination === 'style' ||
+						request.destination === 'script' ||
+						request.destination === 'worker',
+					options: {
+						cacheName: 'static-resources',
+						cacheableResponse: {
+							statuses: [0, 200],
+						},
+					},
+				},
+				// google fonts
+				{
+					handler: 'StaleWhileRevalidate',
+					urlPattern: ({url}) => url.origin === 'https://fonts.googleapis.com',
+					options: {
+						cacheName: 'google-fonts-stylesheets',
+					},
+				},
+				{
+					handler: 'CacheFirst',
+					urlPattern: ({url}) => url.origin === 'https://fonts.gstatic.com',
+					options: {
+						cacheName: 'google-fonts-webfonts',
+						cacheableResponse: {
+							statuses: [0, 200],
+						},
+						expiration: {
+							maxEntries: 30,
+							maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
+						},
+					},
+				},
+			],
+		},
+
 		// to <head/> meta-tags
 		name: 'Explore Spotify',
 		appleMobileWebAppCapable: 'yes',
